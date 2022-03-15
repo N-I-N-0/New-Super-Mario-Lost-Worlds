82360dc297ac1ed9075bbbcd3dfef2cf992955e0
Nin0 <26253311+N-I-N-0@users.noreply.github.com>
2022-03-15 15:22:23 +0100

    save us a midway flag bool

---
+++ Kamek/src/midwayFlag.cpp
@@ -0,0 +1,15 @@
+bool midwayFlagActivated;
+
+void SetMidwayBoolTrue() {
+	midwayFlagActivated = true;
+}
+
+bool SetMidwayBoolTrue2(bool isActive) {
+	midwayFlagActivated = isActive;
+	return isActive;
+}
+
+void SetMidwayBoolFalse() {
+	midwayFlagActivated = false;
+}
+
