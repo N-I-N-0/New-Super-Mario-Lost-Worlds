bd59f942a857d1f536d6f408932a46bff14662d9
Nin0 <26253311+N-I-N-0@users.noreply.github.com>
2022-11-25 21:19:41 +0100

    fix code and add bubble recolors

--- Kamek/src/worldmapManager.cpp
+++ Kamek/src/worldmapManager.cpp
@@ -155,7 +155,7 @@ int dWMManager_c::onCreate() {
 	data[1].len = 0x6;
 	
 	
-	IOS_ioctlv(fd, 8, 0, 2, data); // NCDGetWirelessMacAddress, 32 out, 6 out
+	IOS_Ioctlv(fd, 8, 0, 2, data); // NCDGetWirelessMacAddress, 32 out, 6 out
 	
 	OSReport("Mac: %02X:%02X:%02X:%02X:%02X:%02X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 	IOS_Close(fd);
